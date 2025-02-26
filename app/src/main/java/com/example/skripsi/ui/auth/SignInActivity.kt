7606ed69145a7edf65b4e4ce952f6197e726bfd4
Luthfi Nadhil <ln060303@gmail.com>
2025-02-26 13:28:04 +0700

    added sign in activity

--- app/src/main/java/com/example/skripsi/ui/auth/SignInActivity.kt
+++ app/src/main/java/com/example/skripsi/ui/auth/SignInActivity.kt
@@ -2,11 +2,25 @@ package com.example.skripsi.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.viewModels
 import com.example.skripsi.R
+import com.example.skripsi.data.repository.UserRepository
+import com.example.skripsi.viewmodel.AuthViewModel
+import com.example.skripsi.viewmodel.factory.AuthViewModelFactory
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        val etEmail = findViewById<EditText>(R.id.et_email)
+        val etPassword = findViewById<EditText>(R.id.et_password)
+
+        val btnSignIn = findViewById<Button>(R.id.btn_signin)
+
+        val userRepository = UserRepository()
+        val authViewModel:AuthViewModel by viewModels {AuthViewModelFactory(userRepository)}
     }
 }
