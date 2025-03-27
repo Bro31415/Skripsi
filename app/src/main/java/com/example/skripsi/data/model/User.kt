553e4838c13ea3df1d7d5a2a6020070614817379
William Silitonga <williamsilitonga.miracle@gmail.com>
2025-03-27 11:38:44 +0700

    minor additional wil

--- app/src/main/java/com/example/skripsi/data/model/User.kt
+++ app/src/main/java/com/example/skripsi/data/model/User.kt
@@ -2,10 +2,11 @@ package com.example.skripsi.data.model
 
 import kotlinx.serialization.Serializable
 
+
 @Serializable
 data class User(
     val id: String,
     val username: String,
     val email: String,
-    val xp: Int? = null // Kolom xp diizinkan bernilai null (tambahan wil)
+    val xp: Int? = null, // Kolom xp diizinkan bernilai null (tambahan wil)
 )
