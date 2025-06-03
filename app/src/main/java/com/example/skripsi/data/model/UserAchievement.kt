254a8c8976b778f5a8a32d4c95e78327d709645f
William Silitonga <williamsilitonga.miracle@gmail.com>
2025-06-03 23:57:34 +0700

    minor change

--- app/src/main/java/com/example/skripsi/data/model/UserAchievement.kt
+++ app/src/main/java/com/example/skripsi/data/model/UserAchievement.kt
@@ -3,7 +3,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class UserAchievement(
     val id: Long? = null,
-    val user_id: String,         
+    val user_id: String,
     val achievement_key: String,
     val unlocked_at: String
 )
