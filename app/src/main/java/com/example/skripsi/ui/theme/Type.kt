18f70a9a037db0ca9c9eeae258a0816d5e3cd15a
Luthfi Nadhil <ln060303@gmail.com>
2025-06-09 10:43:11 +0700

    updated course menu

---
+++ app/src/main/java/com/example/skripsi/ui/theme/Type.kt
@@ -0,0 +1,40 @@
+package com.example.skripsi.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.skripsi.R
+
+val PlusJakartaSans = FontFamily(
+    Font(R.font.plusjkt_regular, FontWeight.Normal),
+    Font(R.font.plusjkt_semibold, FontWeight.SemiBold)
+)
+
+val Mattone = FontFamily(
+    Font(R.font.mattone, FontWeight.Normal)
+)
+
+val AppTypography = Typography(
+
+    headlineSmall = TextStyle(
+        fontFamily = Mattone,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+
+    titleMedium = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp
+    ),
+
+    bodySmall = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+
+)
